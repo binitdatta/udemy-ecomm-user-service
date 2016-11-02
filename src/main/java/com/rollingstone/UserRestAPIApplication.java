a6dcdd11870c8ffeb7da126bb0d66aef0de7c1e5
Binit Datta <Binit.Datta@searshc.com>
2016-11-02 11:02:42 -0500

    Added Eureka

--- src/main/java/com/rollingstone/UserRestAPIApplication.java
+++ src/main/java/com/rollingstone/UserRestAPIApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -20,8 +22,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration  // Sprint Boot Automatic Configuration
 @ComponentScan(basePackages = "com.rollingstone")
 @EnableJpaRepositories("com.rollingstone.dao.jpa") // To segregate MongoDB and JPA repositories. Otherwise not needed.
-//@EnableSwagger // This annotation will enable Swagger to auto generate user REST API docs
 @EnableSwagger2
+@EnableDiscoveryClient
+@EnableFeignClients
 public class UserRestAPIApplication extends SpringBootServletInitializer {
 
     private static final Class<UserRestAPIApplication> applicationClass = UserRestAPIApplication.class;
